7ef2dd3de529d0cc2049380724a61fc52a889058
Paula Carneiro <paulacarneiro215@gmail.com>
2023-11-18 11:53:54 +0300

    create wagmi config

--- ui/src/pages/_app.tsx
+++ ui/src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { WagmiConfig, createConfig, configureChains } from "wagmi";
+import { goerli, mainnet } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+const { publicClient, webSocketPublicClient } = configureChains(
+  [mainnet, goerli],
+  [publicProvider()]
+);
+
+const config = createConfig({
+  publicClient,
+  webSocketPublicClient,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={config}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  );
 }
