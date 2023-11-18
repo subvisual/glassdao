ae15c952f752fda6d64b3600717c43be6e4e76d6
Paula Carneiro <paulacarneiro215@gmail.com>
2023-11-18 11:59:30 +0000

    setup thorin
    * wip
    
    * set up thorin
    
    * add satoshi font

--- ui/src/pages/_app.tsx
+++ ui/src/pages/_app.tsx
@@ -4,6 +4,9 @@ import { WagmiConfig, createConfig, configureChains } from "wagmi";
 import { goerli, mainnet } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 import { useIsMounted } from "@/hooks/useIsMounted";
+import { ThorinGlobalStyles, lightTheme } from "@ensdomains/thorin";
+import { ThemeProvider } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
 const { publicClient, webSocketPublicClient } = configureChains(
   [mainnet, goerli],
@@ -15,12 +18,25 @@ const config = createConfig({
   webSocketPublicClient,
 });
 
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: "Satoshi";
+    font-style: normal;
+    font-weight: normal;
+    src: url("/assets/fonts/Satoshi.otf") format("opentype");
+  }
+`;
+
 export default function App({ Component, pageProps }: AppProps) {
   const isMounted = useIsMounted();
 
   return (
     <WagmiConfig config={config}>
-      {isMounted && <Component {...pageProps} />}
+      <ThemeProvider theme={lightTheme}>
+        <ThorinGlobalStyles />
+        <GlobalStyle />
+        {isMounted && <Component {...pageProps} />}
+      </ThemeProvider>
     </WagmiConfig>
   );
 }
--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,14 +1,35 @@
 import Link from "next/link";
 import ConnectMetaMask from "@/components/ConnectMetaMask";
+import { Button, Heading } from "@ensdomains/thorin";
+import styled from "styled-components";
+
+const Main = styled.main`
+  padding: 20px 40px;
+`;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: fit-content;
+  margin: 40px auto;
+`;
 
 export default function Home() {
   return (
-    <div>
-      <h1>Glassdao</h1>
-      <div>
+    <Main>
+      <Heading align="center">GlassDAO</Heading>
+      <Wrapper>
         <ConnectMetaMask />
-      </div>
-      <Link href={"/review"}>Add review</Link>
-    </div>
+        <Button
+          as="a"
+          href="/review"
+          colorStyle="background"
+          shape="rounded"
+          width="45"
+        >
+          Add review
+        </Button>
+      </Wrapper>
+    </Main>
   );
 }
