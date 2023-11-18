7c99cd308c69bf50d99c28f69f25b87561697f56
Paula Carneiro <paulacarneiro215@gmail.com>
2023-11-18 20:08:40 +0300

    fixes

--- ui/src/pages/contributor/index.tsx
+++ ui/src/pages/contributor/index.tsx
@@ -3,12 +3,8 @@ import { useAccount } from "wagmi";
 import { redirect } from "next/navigation";
 import { useUserRole } from "../../hooks/useUserRole";
 
-const Main = styled.main`
-  padding: 20px 40px;
-`;
-
 export default function Contributor() {
   useUserRole();
 
-  return <Main>contributor</Main>;
+  return <>contributor</>;
 }
